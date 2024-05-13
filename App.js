bef8d582f5ddb71123128832648ee9b9ba544c57
GregoryG8 <79380668+GregoryG8@users.noreply.github.com>
2024-05-12 20:42:26 -0500

    Route and navigation

--- App.js
+++ App.js
@@ -1,13 +1,3 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-=======
 import React from "react";
 import Main from "./src/Main";
 import { NavigationContainer } from '@react-navigation/native';
@@ -17,15 +7,7 @@ export default function App() {
     <NavigationContainer>
       <Main />
     </NavigationContainer>
->>>>>>> Stashed changes
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,18 +4,16 @@ import Constants from 'expo-constants';
 
 export default function Home() {
   return (
-    <View style={{marginTop: Constants.statusBarHeight, flexGrow: 1}}>
+    <View style={styles.home}>
       <Text>Hola como estan todos mamawebos</Text>
     </View>
   )
 };
 
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
+const styles = StyleSheet.create({
+  home: {
+    marginTop: Constants.statusBarHeight, 
+    flexGrow: 1
+  },
+});
